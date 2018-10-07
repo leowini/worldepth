5d3ec116a456898ccec93cd049c75ce4f7549977
Leo Winiecki <leodwiniecki@outlook.com>
2018-10-07 15:47:38 -0700

    Set up permissions

---
+++ spikes/GoogleMapsSpike/app/src/main/java/com/example/leodw/googlemapsspike/MapActivity.java
@@ -0,0 +1,16 @@
+package com.example.leodw.googlemapsspike;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class MapActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate( @Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_map);
+
+    }
+}
