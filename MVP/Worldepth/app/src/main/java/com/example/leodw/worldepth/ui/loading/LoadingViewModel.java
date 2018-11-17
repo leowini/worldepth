459c2dbf91b2abcea535e1922ed05cf36a13c405
Leo Winiecki <leodwiniecki@outlook.com>
2018-11-17 13:24:49 -0700

    Got rid of errors for now

--- MVP/Worldepth/app/src/main/java/com/example/leodw/worldepth/ui/loading/LoadingViewModel.java
+++ MVP/Worldepth/app/src/main/java/com/example/leodw/worldepth/ui/loading/LoadingViewModel.java
@@ -1,8 +1,14 @@
 package com.example.leodw.worldepth.ui.loading;
+import android.arch.lifecycle.AndroidViewModel;
 import android.arch.lifecycle.ViewModel;
 import android.databinding.BaseObservable;
 import android.databinding.Bindable;
 
-public class LoadingViewModel extends ViewModel {
+import com.example.leodw.worldepth.ui.MainActivity;
+
+public class LoadingViewModel {
     private static final String TAG = "LoadingViewModel";
+
+    public void backToCamera() {
+    }
 }
