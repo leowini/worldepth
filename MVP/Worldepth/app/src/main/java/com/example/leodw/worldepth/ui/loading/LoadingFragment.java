e00c1df6abf5a3e3520f3d87a74c0fc1eed653ad
Leo Winiecki <leodwiniecki@outlook.com>
2018-11-17 00:10:37 -0700

    added all member viewmodels back

--- MVP/Worldepth/app/src/main/java/com/example/leodw/worldepth/ui/loading/LoadingFragment.java
+++ MVP/Worldepth/app/src/main/java/com/example/leodw/worldepth/ui/loading/LoadingFragment.java
@@ -18,6 +18,8 @@ public class LoadingFragment extends Fragment {
 
     private static final String TAG = "LoadingFragment";
 
+    private LoadingViewModel mLoadingViewModel;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
--- MVP/Worldepth/app/src/main/java/com/example/leodw/worldepth/ui/viewer/ViewerFragment.java
+++ MVP/Worldepth/app/src/main/java/com/example/leodw/worldepth/ui/viewer/ViewerFragment.java
@@ -18,6 +18,8 @@ public class ViewerFragment extends Fragment {
 
     private static final String TAG = "ViewerFragment";
 
+    private ViewerViewModel mViewerViewModel;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
