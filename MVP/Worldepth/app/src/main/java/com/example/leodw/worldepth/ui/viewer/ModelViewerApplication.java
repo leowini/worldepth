bb33cb34fbb53db4f722d40c1b59e07ab2b601f1
Leo Winiecki <leodwiniecki@outlook.com>
2018-11-18 13:41:10 -0700

    Put Johann's stuff into the viewer

---
+++ MVP/Worldepth/app/src/main/java/com/example/leodw/worldepth/ui/viewer/ModelViewerApplication.java
@@ -0,0 +1,35 @@
+package com.example.leodw.worldepth.ui.viewer;
+
+import android.app.Application;
+import android.support.annotation.Nullable;
+
+public class ModelViewerApplication extends Application
+{
+    private static ModelViewerApplication INSTANCE;
+
+    // Store the current model globally, so that we don't have to re-decode it upon
+    // relaunching the main or VR activities.
+    // TODO: handle this a bit better.
+    @Nullable private Model currentModel;
+
+    public static ModelViewerApplication getInstance() {
+        return INSTANCE;
+    }
+
+    @Nullable
+    public Model getCurrentModel() {
+        return currentModel;
+    }
+
+    public void setCurrentModel(@Nullable Model model) {
+        currentModel = model;
+    }
+
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+        INSTANCE = this;
+    }
+}
+
