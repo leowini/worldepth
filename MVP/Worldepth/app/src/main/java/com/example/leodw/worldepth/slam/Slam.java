ebd9e75bf51d83202b536417df2fabd39e04ad61
Leo Winiecki <leodwiniecki@outlook.com>
2018-11-26 16:30:16 -0800

    asdf

--- MVP/Worldepth/app/src/main/java/com/example/leodw/worldepth/slam/Slam.java
+++ MVP/Worldepth/app/src/main/java/com/example/leodw/worldepth/slam/Slam.java
@@ -1,15 +1,17 @@
 package com.example.leodw.worldepth.slam;
 
 import android.graphics.Bitmap;
-import android.graphics.SurfaceTexture;
 
 import com.example.leodw.worldepth.ui.camera.Renderer;
 
 public class Slam implements Renderer.FrameListener {
 
+    public native boolean sendSlam(Bitmap img);
+
     @Override
     public void sendFrameToSlam(Bitmap frame) {
         //JNI call
+
     }
 
 }
