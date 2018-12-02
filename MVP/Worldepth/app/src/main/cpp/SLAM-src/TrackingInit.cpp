b7eb37bdc4a086a6aac079791c6b2868dd6c662b
Michael Duan <michael@Michaels-MacBook-Air.local>
2018-12-02 01:47:35 -0800

    TrackInit Class skeleton started

--- MVP/Worldepth/app/src/main/cpp/SLAM-src/TrackingInit.cpp
+++ MVP/Worldepth/app/src/main/cpp/SLAM-src/TrackingInit.cpp
@@ -3,3 +3,30 @@
 //
 
 #include "TrackingInit.h"
+#include <thread>
+
+namespace SLAM
+{
+    TrackingInit::TrackingInit() {
+        isProcessing = true;
+        std::thread consume (TrackingInit::processing);
+    }
+
+    void TrackingInit::sendToFrameList(SLAM::Frame frame) {
+        FrameList.push_back(frame);
+    }
+
+    void TrackingInit::processing() {
+        while (isProcessing) {
+            TrackingInit::sendToProcess();
+        }
+    }
+
+    void TrackingInit::sendToProcess() {
+        Frame coolFrame = FrameList[0];
+
+        //call function on FrameList[0]
+
+        FrameList.erase(FrameList.begin());
+    }
+}
