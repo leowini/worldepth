7483a9535026bcd9a62f99bcacf9d54cc24b8dda
Leo Winiecki <leodwiniecki@outlook.com>
2018-12-12 12:29:17 -0800

    Altered the SectionsStatePagerAdapter

--- MVP/Worldepth/app/src/main/java/com/example/leodw/worldepth/ui/MainActivity.java
+++ MVP/Worldepth/app/src/main/java/com/example/leodw/worldepth/ui/MainActivity.java
@@ -19,8 +19,8 @@ import com.example.leodw.worldepth.ui.viewer.ViewerFragment;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-
-    private SectionsStatePagerAdapter mSectionsStatePagerAdapter;
+    
+    private SectionsStatePagerAdapter mPagerAdapter;
     private ViewPager mViewPager;
     static private FirebaseWrapper fb;
 
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mSectionsStatePagerAdapter = new SectionsStatePagerAdapter(getSupportFragmentManager());
+        mPagerAdapter = new SectionsStatePagerAdapter(getSupportFragmentManager());
         mViewPager = (ViewPager) findViewById(R.id.container);
         setupViewPager(mViewPager);
         fb = new FirebaseWrapper();
@@ -42,21 +42,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setupViewPager(ViewPager viewPager) {
-        SectionsStatePagerAdapter adapter = new SectionsStatePagerAdapter(getSupportFragmentManager());
-        adapter.addFragment(new SignUpFragment(), "SignUp_Fragment");
-        adapter.addFragment(new PhoneFragment(), "Phone_Fragment");
-        adapter.addFragment(new BirthdayFragment(), "Birthday_Fragment");
-        adapter.addFragment(new NameFragment(), "Name_Fragment");
-        adapter.addFragment(new EmailFragment(), "Email_Fragment");
-        adapter.addFragment(new LoginFragment(), "Login_Fragment");
-        adapter.addFragment(new CameraFragment(), "Camera_Fragment");
-        adapter.addFragment(new LoadingFragment(), "Loading_Fragment");
-        adapter.addFragment(new ViewerFragment(), "Viewer_Fragment");
-        viewPager.setAdapter(adapter);
+        mPagerAdapter.addFragment(new SignUpFragment(), "SignUp_Fragment");
+        mPagerAdapter.addFragment(new PhoneFragment(), "Phone_Fragment");
+        mPagerAdapter.addFragment(new BirthdayFragment(), "Birthday_Fragment");
+        mPagerAdapter.addFragment(new NameFragment(), "Name_Fragment");
+        mPagerAdapter.addFragment(new EmailFragment(), "Email_Fragment");
+        mPagerAdapter.addFragment(new LoginFragment(), "Login_Fragment");
+        mPagerAdapter.addFragment(new CameraFragment(), "Camera_Fragment");
+        mPagerAdapter.addFragment(new LoadingFragment(), "Loading_Fragment");
+        mPagerAdapter.addFragment(new ViewerFragment(), "Viewer_Fragment");
+        viewPager.setAdapter(mPagerAdapter);
     }
 
     public void setViewPager(int fragmentIndex) {
         mViewPager.setCurrentItem(fragmentIndex);
+    }
+
+    public void setViewPagerByTitle(String fragmentTitle) {
+        int fragmentIndex = mPagerAdapter.getFragmentNumber(fragmentTitle);
+        setViewPager(fragmentIndex);
     }
 
     public FirebaseWrapper getFirebaseWrapper(){
