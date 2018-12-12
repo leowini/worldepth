26f66818fafec4075c7ac89765cf65b4564f9149
Leo Winiecki <leodwiniecki@outlook.com>
2018-12-12 10:10:31 -0800

    Filled in fragments with stuff

--- MVP/Worldepth/app/src/main/java/com/example/leodw/worldepth/ui/signup/Email/EmailFragment.java
+++ MVP/Worldepth/app/src/main/java/com/example/leodw/worldepth/ui/signup/Email/EmailFragment.java
@@ -1,6 +1,46 @@
 package com.example.leodw.worldepth.ui.signup.Email;
 
+import android.arch.lifecycle.ViewModelProviders;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.leodw.worldepth.R;
+import com.example.leodw.worldepth.data.FirebaseWrapper;
+import com.example.leodw.worldepth.ui.MainActivity;
+import com.example.leodw.worldepth.ui.signup.Name.NameViewModel;
+import com.example.leodw.worldepth.ui.signup.Phone.PhoneFragment;
 
 public class EmailFragment extends Fragment {
+    private static final String TAG = "PhoneFragment";
+
+    private EmailViewModel mViewModel;
+    private FirebaseWrapper mFb;
+
+
+    public static EmailFragment newInstance() {
+        return new EmailFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.email_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(EmailViewModel.class);
+        mFb = ((MainActivity)this.getActivity()).getFirebaseWrapper();
+        // TODO: Use the ViewModel
+    }
+
+    @Override
+    public void onViewCreated(final View view, Bundle savedInstanceState) {
+    }
 }
