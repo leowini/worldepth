6a7c6b9b362592e548dff544285ce2dbcc43989c
Soren Dahl <sorendahl@Sorens-MacBook-Pro.local>
2018-12-27 15:13:11 -0800

    almost finished with commented skeletons for SLAM, rewriting map writer

--- MVP/Worldepth/app/src/main/cpp/SLAM-src/RunSlam.cpp
+++ MVP/Worldepth/app/src/main/cpp/SLAM-src/RunSlam.cpp
@@ -2,5 +2,25 @@
 // Created by Soren Dahl on 12/17/18.
 //
 
-void run
+#include <RunSlam.h>
+#include <TrackingInit.h>
+
+namespace SLAM {
+
+    void start () {
+        TrackingInit();
+        //maybe do other threading here
+    }
+
+    void process(cv::Mat im, double tstamp) {
+
+        //call the equivalent of System::TrackMonocular for TrackingInit or Tracking directly
+    }
+
+
+    void end () {
+        //get
+    }
+}
+
 
