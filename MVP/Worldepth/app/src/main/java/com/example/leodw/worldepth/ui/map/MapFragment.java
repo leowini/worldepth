598354474f31c5e9b51f7da93e2f879129c30e3a
Leo Winiecki <leodwiniecki@outlook.com>
2018-12-30 17:15:18 -0800

    MapActivity is working

--- MVP/Worldepth/app/src/main/java/com/example/leodw/worldepth/ui/map/MapFragment.java
+++ MVP/Worldepth/app/src/main/java/com/example/leodw/worldepth/ui/map/MapFragment.java
@@ -10,6 +10,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.example.leodw.worldepth.R;
+import com.example.leodw.worldepth.ui.MainActivity;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -35,6 +36,7 @@ public class MapFragment extends Fragment {
         Button mMessageButton = view.findViewById(R.id.mapToMessageBtn);
         Button mCameraButton = view.findViewById(R.id.mapToCameraBtn);
         Button mGoToMapActivity = view.findViewById(R.id.fakeMapToMapBtn);
+        mGoToMapActivity.setOnClickListener(v -> ((MainActivity) getActivity()).switchToMap());
         mProfileButton.setOnClickListener(v -> Navigation.findNavController(v).navigate(R.id.action_mapFragment_to_profileFragment));
         mCameraButton.setOnClickListener(v -> Navigation.findNavController(v).navigate(R.id.action_mapFragment_to_cameraFragment));
         mMessageButton.setOnClickListener(v -> Navigation.findNavController(v).navigate(R.id.action_mapFragment_to_messageFragment));
