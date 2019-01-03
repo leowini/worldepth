a8b42bb1b08ff3dd92c7bac09020b138f708c59c
Leo Winiecki <leodwiniecki@outlook.com>
2019-01-02 18:07:33 -0800

    asdf

--- MVP/Worldepth/app/src/main/java/com/example/leodw/worldepth/ui/profile/AddFriendFragment.java
+++ MVP/Worldepth/app/src/main/java/com/example/leodw/worldepth/ui/profile/AddFriendFragment.java
@@ -1,6 +1,7 @@
 package com.example.leodw.worldepth.ui.profile;
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -10,6 +11,13 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.example.leodw.worldepth.R;
+import com.example.leodw.worldepth.ui.MainActivity;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -25,6 +33,32 @@ public class AddFriendFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        DatabaseReference dbRef = ((MainActivity) getActivity()).getFirebaseWrapper().getFirebaseDatabase().getReference();
+        ArrayList<String> userList = new ArrayList<>();
+        dbRef.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (//) {
+                    User user =
+                    userList.append(user.firstName);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        myRef.child(phoneNo).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                user = dataSnapshot.getValue(User.class);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {}
+
+        });
+
         mBackToProfileButton = view.findViewById(R.id.friendToProfileBtn);
         mBackToProfileButton.setOnClickListener(v -> Navigation.findNavController(v).navigate(R.id.action_addFriend_to_profileFragment));
     }
