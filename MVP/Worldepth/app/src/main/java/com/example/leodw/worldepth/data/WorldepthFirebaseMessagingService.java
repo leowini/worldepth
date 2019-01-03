e5ce91b4cd04764ac16b104e4db39b70b0aa73f0
Leo Winiecki <leodwiniecki@outlook.com>
2019-01-02 18:17:54 -0800

    Added firebase-messaging to the build.gradle file

---
+++ MVP/Worldepth/app/src/main/java/com/example/leodw/worldepth/data/WorldepthFirebaseMessagingService.java
@@ -0,0 +1,7 @@
+package com.example.leodw.worldepth.data;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class WorldepthFirebaseMessagingService extends FirebaseMessagingService {
+    
+}
