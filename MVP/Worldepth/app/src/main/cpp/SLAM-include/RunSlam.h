9d87894c0f6d3c8c7bcbdf37847dd648a17194e0
Soren Dahl <sorendahl@Sorens-MacBook-Pro.local>
2019-01-10 09:50:36 -0800

    turned slam into a runslam variable so it isn't passed

--- MVP/Worldepth/app/src/main/cpp/SLAM-include/RunSlam.h
+++ MVP/Worldepth/app/src/main/cpp/SLAM-include/RunSlam.h
@@ -13,15 +13,17 @@ namespace SLAM
 
 //call this fist to start the TrackingInit and threads,
 //we can remove the settings file, but I need to know how to access the phone's settings
-System* start(std::string &vocFile, std::string &settingsFile);
+void start(std::string &vocFile, std::string &settingsFile);
 
 //call this for each frame
-void process(System * slam, cv::Mat &im, double tstamp);
+void process(cv::Mat &im, double tstamp);
 
 //call this at the end, writes the map to the file and closes things
 //for internal storage, this looks like "/data/data/com.example.leodw.worldepth/name of file" i think
 //This could then read into reconstruction or go to the database so it isn't stored on the phone
-void end(System * slam, std::string filename);
+void end(std::string filename);
+
+System *slam;
 
 } //namespace SLAM
 
--- MVP/Worldepth/app/src/main/cpp/SLAM-src/RunSlam.cpp
+++ MVP/Worldepth/app/src/main/cpp/SLAM-src/RunSlam.cpp
@@ -10,20 +10,20 @@
 namespace SLAM
 {
 
-    System* start (std::string & vocFile, std::string & settingsFile) {
-        System* slam = new System(vocFile, settingsFile);
-        return slam;
+    void start (std::string & vocFile, std::string & settingsFile) {
+        slam = new System(vocFile, settingsFile);
+        //return slam;
     }
 
     //I still don't know how to do the initializer, if it's not automatically Idk how this is done
-    void process(System * slam, cv::Mat im, double tstamp) {
+    void process(cv::Mat im, double tstamp) {
         //call the equivalent of System::TrackMonocular for TrackingInit or Tracking directly
         if (im.empty() || tstamp == 0){
             cerr << "could not load image!" << endl;
         } else if (im.rows == 1 && im.cols == 1) {  //poison pill
             //this makes and writes to an internal storage file, reused every time slam is run
             //we may change this to instead pass it to Poisson
-            end(slam, "data/user/0/com.example.leodw.worldepth/files/SLAM.txt");
+            end("data/user/0/com.example.leodw.worldepth/files/SLAM.txt");
         } else {
             slam->TrackMonocular(im, tstamp);
         }
@@ -31,13 +31,11 @@ namespace SLAM
     }
 
 
-    void end (System *slam, std::string filename) {
+    void end (std::string filename) {
 
         //get finished map as reference
         writeMap(filename, slam->GetTrackedMapPoints());
-
-        //this should run map.clear and delete map
-        //delete slam;
+        
 
         slam->Shutdown();
         //System actually has a clear func, it's
