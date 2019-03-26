f45e9a7c51db56cd9d48292d193821b9124d5c97
Leo Winiecki <leodwiniecki@outlook.com>
2019-03-26 13:28:11 -0700

    Creating reconstructor class

---
+++ Worldepth/app/src/main/cpp/Reconstructor.h
@@ -0,0 +1,14 @@
+#ifndef RECONSTRUCTOR_H
+#define RECONSTRUCTOR_H
+
+#include <opencv2/core/mat.hpp>
+
+class Reconstructor {
+public:
+
+private:
+    std::vector<cv::Mat> *vKFImColor;
+    std::vector<cv::Mat> *vKFTcw;
+};
+
+#endif
