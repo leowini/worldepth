234e10f755aa741d0398802f550337a3daafd222
Leo Winiecki <leodwiniecki@outlook.com>
2019-03-30 16:45:59 -0700

    Not deleting keyframes and poses until after textureMapping completes

--- Worldepth/app/src/main/cpp/Reconstructor.cpp
+++ Worldepth/app/src/main/cpp/Reconstructor.cpp
@@ -33,10 +33,10 @@ void Reconstructor::endSlam(std::string filename, bool slamSuccess) {
     //close any other threads (should be done already in System.Reset()
     delete slam;
     if (slamSuccess) textureMapper = new TextureMapper("/data/user/0/com.example.leodw.worldepth/files/Horse.ply", vKFImColor, vKFTcw);
-    delete vKFImColor;
-    delete vKFTcw;
 }
 
 void Reconstructor::textureMap() {
     textureMapper->textureMap();
+    delete vKFImColor;
+    delete vKFTcw;
 }
