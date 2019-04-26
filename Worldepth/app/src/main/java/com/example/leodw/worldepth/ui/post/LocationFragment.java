ce98600a5d87d8318fef680ab56486fc4badb77e
Soren Dahl <sorendahl@Sorens-MacBook-Pro.local>
2019-04-26 09:45:34 -0700

    made new location fragment

---
+++ Worldepth/app/src/main/java/com/example/leodw/worldepth/ui/post/LocationFragment.java
@@ -0,0 +1,130 @@
+package com.example.leodw.worldepth.ui.post;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.navigation.Navigation;
+
+import com.example.leodw.worldepth.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link LocationFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link LocationFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class LocationFragment extends Fragment implements OnMapReadyCallback, GoogleMap.OnMapClickListener {
+
+    private GoogleMap mMap;
+
+    private OnFragmentInteractionListener mListener;
+
+    private Button mbackButton;
+    private Button mPostButton;
+
+    public LocationFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     *
+     * @return A new instance of fragment LocationFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static LocationFragment newInstance(String param1, String param2) {
+        LocationFragment fragment = new LocationFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_location, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        mbackButton = view.findViewById(R.id.postBackButton);
+        mPostButton = view.findViewById(R.id.postButton);
+        mbackButton.setOnClickListener(v -> Navigation.findNavController(v).navigate(R.id.action_locationFragment_to_viewerFragment));
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        googleMap.setOnMapClickListener((this));
+    }
+
+    @Override
+    public void onMapClick(LatLng loc){
+        mMap.clear();
+        mMap.addMarker(new MarkerOptions().position(loc).title("post location"));
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
