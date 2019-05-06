95ff92fa459c42cd4675e8f6e1afa8e37a84456e
Soren Dahl <sorendahl@Sorens-MacBook-Pro.local>
2019-05-06 14:53:48 -0700

    tried to make tracking easier

--- Worldepth/app/src/main/cpp/SLAM-src/Tracking.cpp
+++ Worldepth/app/src/main/cpp/SLAM-src/Tracking.cpp
@@ -381,7 +381,7 @@ namespace SLAM
                 if(mpMap->KeyFramesInMap()<=5)
                 {
                     cout << "Track lost soon after initialisation, resetting..." << endl;
-                    //mpSystem->Reset();
+                    mpSystem->Reset();
                     return;
                 }
             }
